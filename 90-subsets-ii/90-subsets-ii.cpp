f89bd0b863a3d677d5885dde6eb363a5c13a14f1
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-16 00:19:29 +0530

    Time: 34 ms (5.07%), Space: 24.3 MB (5.39%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    void solve(vector<int> &nums, vector<vector<int>> &ans, int ind, vector<int> ds, map<int, int> indices){
+        if(ind == nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        int ind2 = indices[nums[ind]]+1;
+        for(int i = ind-1; i < ind2; i++){
+            if(i>=ind){
+                ds.push_back(nums[ind]);
+                solve(nums, ans, ind2, ds, indices);
+            }else{
+                solve(nums, ans, ind2, ds, indices);
+            }
+        }
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        map<int, int> indices;
+        for(int i=0; i<nums.size(); i++){
+            indices[nums[i]] = i;
+        }
+        vector<int> ds;
+        solve(nums, ans, 0, ds, indices);
+        return ans;
+    }
+};
