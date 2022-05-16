3ecfbb81b6c3db104d4fccae9c3a2b37f73d7bd8
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-16 12:25:53 +0530

    Time: 7 ms (30.72%), Space: 6.1 MB (48.88%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<bool> numAvailable(n+1, true);
+        vector<int> impact(n+1, 1);
+        
+        impact[n] =1;
+        for(int i=n-1; i>=1; i--) impact[i] = impact[i+1]*(n-i+1);
+        
+        string ans;
+        k--;
+        for(int ind=1; ind<=n; ind++){
+            int posFind = 1;
+            if(k==0)  posFind = 1;
+            else posFind = k/impact[ind+1]+1;
+            
+          //  cout << posFind << " " << numFind << " " << k << endl;
+            
+            int numFind = 1;
+            
+            int cnt = 0;
+            for(int num = 1; num <= n; num++){
+                if(numAvailable[num]) cnt++;
+                
+                if(cnt >= posFind){
+                    numFind = num;
+                    break;
+                }
+            }
+            
+            ans.push_back(numFind+'0');
+            numAvailable[numFind] = false;
+            
+            if(k > 0)
+            k = k%impact[ind+1];
+            
+            //cout << posFind << " " << numFind << " " << k << endl;
+        }
+        
+        return ans;
+    }
+};
