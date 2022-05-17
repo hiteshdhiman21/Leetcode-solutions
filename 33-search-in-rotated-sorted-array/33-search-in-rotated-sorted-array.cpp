0dc3271402b1c54a290ceee7505867d2fc997c61
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-17 15:48:36 +0530

    Time: 7 ms (44.11%), Space: 11.1 MB (29.79%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    int searchIn(vector<int> &nums, int start, int end, int target){
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(nums[mid] < target){
+                start = mid+1;
+            }else if(nums[mid] > target){
+                end = mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        
+         //First find index of rotation
+        //Using it, we can convert the given array into two sorted arrays.
+        //Use binary search separately on both the arrays.
+        
+        int n = nums.size();
+        int indOfR = n;
+        int start = 0, end = n-1;
+                
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(nums[mid] >= nums[0]){
+                start = mid+1;
+            }else{
+                indOfR = mid;
+                end = mid-1;
+            }
+        }
+        
+        int ind1 = searchIn(nums, 0, indOfR-1, target);
+        int ind2 = searchIn(nums, indOfR, n-1, target);
+       
+        
+        if(ind2 != -1) return ind2;
+        else return ind1;
+        
+        //T - 2logn             
+        //S - O(1)
+    }
+};
