b54ea38abdf77a3a3bda9f34df6990d30c7bb253
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-20 23:57:56 +0530

    Time: 3 ms (80.74%), Space: 8 MB (13.17%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        
+        //Just use DP
+        //if(grid[i][j] == 0), dp[i][j] = dp[i+1][j]+dp[i][j+1]
+        
         int n = grid.size(), m = grid[0].size();
         vector<vector<long long>> dp(n, vector<long long>(m, 0));
         
@@ -8,24 +12,23 @@ public:
         for(int i=n-2; i>=0; i--){
             if(grid[i][m-1] == 0)
             dp[i][m-1] = dp[i+1][m-1];
-            else
-            dp[i][m-1] = 0;
         }
         
-        for(int i=m-2; i>=0; i--){
-            if(grid[n-1][i] == 0)
-            dp[n-1][i] = dp[n-1][i+1];
-            else
-            dp[n-1][i] = 0;
+        for(int j=m-2; j>=0; j--){
+            if(grid[n-1][j] == 0)
+            dp[n-1][j] = dp[n-1][j+1]; 
         }
         
         for(int i=n-2; i>=0; i--){
             for(int j=m-2; j>=0; j--){
                 if(grid[i][j] == 0)
                     dp[i][j] = dp[i+1][j]+dp[i][j+1];
+                //Otherwise already setted zero
             }
         }
         
         return dp[0][0];
+        //T - O(m*n)
+        //S - O(m*n)
     }
 };
