33a69d6438b3eea1dbd913eedb70361a0d0eeae6
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-21 10:11:26 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.82%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -7,6 +7,14 @@ public:
         return '0';
     }
     
+    //for ch in s:
+    //  if(ch is opening parenthesis) push into stack
+    //  else if(st is not empty and st.top() == exactMatch(ch)) pop
+    //  else return false;  //Because there is no opening for this closing parenthesis
+    
+    //Finally if(st.size() == 0) return true
+    //        else return false //As there is no closing parenthesis for parenthesis pushed into stack
+    
     bool isValid(string s) {
         stack<int> st;
         for(char ch:s){
