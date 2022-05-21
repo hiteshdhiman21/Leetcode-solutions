159b542d92125f47bc6be2919f6b6f6a59ef5bb2
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-21 09:51:00 +0530

    Time: 3 ms (41.47%), Space: 7.1 MB (13.62%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,38 +1,52 @@
 class MyQueue {
 
-stack<int> st1, st2;
+stack<int> in; //Will store in LIFO order
+stack<int> out; //Will store in FIFO order
     
 public:
     MyQueue() {
     }
     
     void push(int x) {
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
-        }  
-        
-        st1.push(x);
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
-        
+        in.push(x);
     }
     
+    //Here out will store in the correct FIFO order. So if out is not empty we will just return from its top.
+    //Else we will transfer all the elements from in to out.(Elements inserted in in:1 2 3 4 5), Elements coming out from in: 5 4 3 2 1. Elements going to out: 5 4 3 2 1. So element coming out from out: 1 2 3 4 5(same as the input order)
+    //fill in : 1 2 3
+    //empty in: LIFO - 3 2 1
+    //fill out: order - 3 2 1 
+    //empty out: LIFO - 1 2 3
     int pop() {
-        int x = st1.top();
-        st1.pop();
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        
+        int x = out.top();
+        out.pop();
         return x;
     }
     
     int peek() {
-        return st1.top();
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        return out.top();
     }
     
     bool empty() {
-        return st1.size() == 0;
+        return in.size() == 0 && out.size() == 0;
     }
+    
+    //T - Push(1), Pop(am_1), Top(am_1)
+    //S - O(n)
 };
 
 /**
