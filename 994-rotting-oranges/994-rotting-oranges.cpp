1e54c6d210efbb983f9e43bd9ae563934262c177
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-23 23:43:24 +0530

    Time: 14 ms (32.20%), Space: 14 MB (10.93%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     
-    void bfs(vector<vector<int>>& grid, vector<vector<int>>& time, int i, int j, int timecnt){
-        //cout << i << " " << j << endl;
-        if(i == -1 || j==-1 || i==grid.size() || j==grid[0].size())
-            return;
-        
-        if(grid[i][j] == 0 || time[i][j] <= timecnt) return;
-        time[i][j] = timecnt;
-        
-        vector<int> dx({0, 0, -1, +1});
-        vector<int> dy({-1, +1, 0, 0});
-        
-        for(int d=0; d<4; d++){
-            bfs(grid, time, i+dx[d], j+dy[d], timecnt+1);
-        }
-    }
+   
     
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> time(n, vector<int>(m, INT_MAX));
         
+        queue<pair<int, int>> q;
+        
+        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                //cout <<i << " " << j << endl;
-                if(grid[i][j] == 2 && time[i][j] != 0){
-                    bfs(grid, time, i, j, 0);
+                if(grid[i][j] == 2){
+                    q.push(make_pair(i, j));
+                    time[i][j] = 0;
                 }else if(grid[i][j] == 0){
                     time[i][j] = 0;
                 }
             }
         }
+        
+        while(!q.empty()){
+            pair<int,int> pr = q.front(); q.pop();
+            int x = pr.first, y = pr.second;
+            
+            vector<int> dx({0, 0, -1, +1});
+            vector<int> dy({-1, +1, 0, 0});
+            for(int  d= 0; d<4; d++){
+                int nx = x+dx[d], ny = y+dy[d];
+                if(nx == -1 || ny == -1 || nx == n || ny==m || time[nx][ny]<=time[x][y]) continue;
+                
+                time[nx][ny] = time[x][y]+1;
+                q.push(make_pair(nx, ny));
+            }
+            
+        }
+        
         int ans = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
@@ -40,7 +46,6 @@ public:
         
         if(ans == INT_MAX)
         return -1;
-        else
         return ans;
     }
 };
