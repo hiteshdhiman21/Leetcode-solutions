3b642ecab60922b0bc2eb1d6d2779a9a50584ae3
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-23 11:40:02 +0530

    Time: 191 ms (79.76%), Space: 75.2 MB (95.97%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -2,53 +2,32 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         
+        //Its a over-optimised solution with decreasing constant factors in complexity
+        //In this approach we are calculating ans for the elements in stack which are to be removed. i.e. for which the right small is ind and left small is the index just below it in the stack.
+        //Hence we iterate till index n to remove all the elements.
         //Intuition: Compute the left and right border for each index upto which the height is atleast heights[ind] to calculate width[ind], otherwise the rectangle with heights[ind] can't be formed after those indexes. And for each do ans = max(ans, heights[ind]*width[ind])
         //Step-1. First we compute leftSmall and rightSmall height indexes for each height index using PSE, NSE.
         //Step-2. Then we iterate over the heights array and for each index computes area = heights[ind]*width[ind], where width[ind] = rightSmall[ind]-leftSmall[ind]-1.
         
         int n = heights.size();
-        vector<int> leftSmall(n);
-        vector<int> rightSmall(n);
-        
-        stack<pair<int, int>> st;
-        //Using Next Greater element finding technique to find PSE and NSE for each index.
-        for(int i=0; i<n; i++){
-            while(!st.empty() && st.top().first >= heights[i]){
-                st.pop();
-            }
-            
-            if(st.empty()) leftSmall[i] = -1;
-            else{
-                leftSmall[i] = st.top().second;
-            }
-            
-            st.push(make_pair(heights[i], i));
-        }
-        
-        while(!st.empty()) st.pop();
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && st.top().first >= heights[i]){
-                st.pop();
-            }
-            
-            if(st.empty()) rightSmall[i] = n;
-            else{
-                rightSmall[i] = st.top().second;
-            }
-            
-            st.push(make_pair(heights[i], i));
-        }
-        
-        
         
         int ans = 0;
-        for(int i=0; i<n; i++){
-            int width = rightSmall[i]-leftSmall[i]-1;
-            ans = max(ans, heights[i]*width);
-        }
+        stack<int> st;
+        //Using Next Greater element finding technique to find PSE and NSE for each index.
         
+        for(int i=0; i<=n; i++){
+            while(!st.empty() && (i==n || heights[st.top()] >= heights[i])){
+                int height = heights[st.top()]; st.pop();
+                int width = i;
+                if(!st.empty()) width = i - st.top()-1;
+                ans = max(ans, height*width);
+            }
+            
+            st.push(i);
+        }
         return ans;
-        //T - 5n    (3 Passes)
-        //S - O(2n)
+        
+        //T - 2n   (1-pass)
+        //S - O(n)  (1-stack only)
     }
 };
