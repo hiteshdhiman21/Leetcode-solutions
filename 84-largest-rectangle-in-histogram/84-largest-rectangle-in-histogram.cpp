436d4bc54341dbe9f2153422557f39946074f708
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-23 11:06:21 +0530

    Time: 237 ms (49.30%), Space: 82.1 MB (30.59%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        
+        //Intuition: Compute the left and right border for each index upto which the height is atleast heights[ind] to calculate width[ind], otherwise the rectangle with heights[ind] can't be formed after those indexes. And for each do ans = max(ans, heights[ind]*width[ind])
+        //Step-1. First we compute leftSmall and rightSmall height indexes for each height index using PSE, NSE.
+        //Step-2. Then we iterate over the heights array and for each index computes area = heights[ind]*width[ind], where width[ind] = rightSmall[ind]-leftSmall[ind]-1.
+        
         int n = heights.size();
         vector<int> leftSmall(n);
         vector<int> rightSmall(n);
         
         stack<pair<int, int>> st;
+        //Using Next Greater element finding technique to find PSE and NSE for each index.
         for(int i=0; i<n; i++){
             while(!st.empty() && st.top().first >= heights[i]){
                 st.pop();
