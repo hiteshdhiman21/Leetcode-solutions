5d1a800bddd60d6fffa2466432b4dc899162d51f
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-24 22:54:06 +0530

    Time: 3 ms (56.18%), Space: 8.4 MB (76.36%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/* Recursive
 class Solution {
 public:
     void inorder(TreeNode *root, vector<int> &ans){
@@ -22,6 +23,40 @@ public:
         //Approach: Left Self Right
         vector<int> ans;
         inorder(root, ans);
+        return ans;
+    }
+    //T - O(n)
+    //S - O(h) = O(n) in case of skewed trees
+    
+};*/
+
+//Iterative
+class Solution {
+public:
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        //Approach: Left Self Right (Use stack for simulating recursion)
+        //Step-1. Use while loop and start from the root
+        //Step-2. For each node push it into the stack and go to its left node.
+        //Step-3. If node == NULL, set node = pop from the stack (it will directly reach to the closest ancestor which is not printed). Then do similar steps on that node.
+        //Step-4. Once stack is empty it means every node is printed along with its subtrees. hence break.
+        
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode *cur = root;
+        
+        while(true){
+            if(cur != NULL){
+                st.push(cur);
+                cur = cur->left;
+            }else{
+                if(st.empty()) break;
+                cur = st.top(); st.pop();
+                ans.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        
         return ans;
     }
     //T - O(n)
