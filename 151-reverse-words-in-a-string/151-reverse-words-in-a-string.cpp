73c55b7372c4a202fb4e65cbc8f0f74c1c89f341
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-24 13:35:34 +0530

    Time: 4 ms (79.75%), Space: 7.3 MB (72.14%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        //Step-1. Starting from the last and trying to find end and start for each word if present.
+        //Step-2. If found, appending that word(substring) to the ans with a ' '
         string ans;
         int ecr = s.size()-1;
         for(int i=s.size()-1; i>=0; i--){
@@ -11,10 +13,12 @@ public:
                 }
             }
             
-            if(s[i] == ' ') ecr = i-1;
+            if(i>0 && s[i] == ' ' && s[i-1] != ' ') ecr = i-1;
         }
         
         ans.pop_back();
         return ans;
     }
+    //T - O(n)
+    //S - O(n) for making ans
 };
