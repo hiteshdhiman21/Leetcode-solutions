d7012a9c679d2e731a0b70938d3ba41a78e161b5
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-24 19:42:04 +0530

    Time: 38 ms (51.67%), Space: 38.2 MB (21.50%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -2,19 +2,28 @@ class Solution {
 public:
     int solve(vector<int>& nums, int ind, int target, vector<vector<int>> &dp){
         if(ind == nums.size()){
+            //It means target is 0
             if(target == 1000) return 1;
             else return 0;
         } 
         
         if(dp[ind][target] != -1) return dp[ind][target];
         
-        int ans = solve(nums, ind+1, target-nums[ind], dp); //If used positive sign
-        if(target+nums[ind] <= 2000)
-        ans += solve(nums, ind+1, target+nums[ind], dp);   //If used negative sign
+        //If used positive sign before nums[ind]
+        int ans = solve(nums, ind+1, target-nums[ind], dp); 
+        
+        //If used negative sign before nums[ind]
+        if(target+nums[ind] <= 2000)  //else greater than 2000(real : 1000) target is not possible according to constraints        
+        ans += solve(nums, ind+1, target+nums[ind], dp);   
+        
         return dp[ind][target] = ans;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
+        //Just shift the origin from 0 to 1000. 
+        //-1000 -> 0
+        // 0 -> 1000
+        //1000 -> 2000
         int n = nums.size();
         vector<vector<int>> dp(nums.size(), vector<int>(2005, -1));
         return solve(nums, 0, target+1000, dp);
