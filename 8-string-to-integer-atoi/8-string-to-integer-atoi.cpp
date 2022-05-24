92b4df1f08829564dabb10be0fdf17b799495d0a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-24 19:22:50 +0530

    Time: 4 ms (64.50%), Space: 7 MB (89.91%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     int myAtoi(string s) {
        
+        //Step-1. Take 'i' to just bypass the leading spaces and sign if any.
+        //Step-2. Now start from the back to just before the spaces and sign at the start and start converting into interger (as we can see from the end if it is valid or not. If not valid we can just make ans = 0 and restart from before that index). It will also help in maintaining mul_fac as required.
+        
+        //Step-3. Beware of cases like positive or negative integer overflow. Two signs.  spaces between the numbers.
+        //Cases : _4, _-+45, _+4 76, _4word are therein, _+0923099120202_9.
+        
         int i =0;
         while(i<s.size()){
             if(s[i] != ' ') break;
@@ -20,24 +26,24 @@ public:
         long long mul_fac = 1;
         
         for(int j = s.size()-1; j>=i; j--){
-            if(!isdigit(s[j])){
+            if(!isdigit(s[j])){ //If invalid just reset the ans and mul_fac again
                 ans = 0;
                 mul_fac = 1;
             }else{
-            
                 int dig = s[j]-'0';
-                if(ans <= INT_MAX)
-                ans += dig*mul_fac;
                 
-                
-                if(mul_fac != 1e10) mul_fac  *= 10;
+                //Below steps are To prevent overflow of long long and still reach INT_MAX 
+                if(ans <= INT_MAX) 
+                ans += dig*mul_fac; 
+
+                if(mul_fac != 1e10) mul_fac  *= 10; 
             }
         }
         
         if(sign == -1){
-            if(ans > INT_MAX) ans = INT_MIN;
-            else ans = -ans;
-            return max((long long)INT_MIN, ans);
+            if(ans > INT_MAX) ans = INT_MIN;//more than INT_MAX have to convert into INT_MIN
+            else ans = -ans; //we just have to negate the sign for <= INT_MAX
+            return ans;
         }
         
         return min((long long)INT_MAX, ans);
