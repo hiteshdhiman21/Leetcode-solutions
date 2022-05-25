c0943a8987a27ec486375f7c3c2aad145fbf198a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-25 11:33:52 +0530

    Time: 3 ms (55.37%), Space: 8.3 MB (93.82%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -12,28 +12,38 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        //Approach: Morris Traversal
+        //Step-1. While cur is not empty traverse. (Fact: Always leftmost node is visited first and rightmost node is visited last)
+        //Case-1: If(cur doesn't have left), Just print the cur and move to right child.
+        //Case-2: If(cur have left), Go to the rightmost node in the left subtree and if its right link is NULL(it means left subtree is not visited yet), make its right link to the cur, print cur and move to the left child.
+        //If its right link is pointing to the cur,(it means we came here after visiting left subtree), so just remove the link, and move to the right subtree.
         
-        //Similar to inorder.
-        //Here if cur is not null we first print it and then go to the left child. else, cur = st.pop() and cur = cur->right
         vector<int> ans;
-        
-        TreeNode* cur = root;
-        stack<TreeNode*> st;
-        
-        while(true){           
-            if(cur != NULL){
+        TreeNode *cur = root;
+         
+        while(cur){
+            if(!cur->left){  //If cur doesn't have left, just print this and go to right then.
                 ans.push_back(cur->val);
-                st.push(cur);
-                cur = cur->left;
-            }else{
-                if(st.empty()) break;
-                cur = st.top(); st.pop();
                 cur = cur->right;
+            }else{
+                //Go to rightmost node in the leftsubtree and make it right link point to cur.
+                TreeNode *prev = cur->left;
+                while(prev->right != NULL && prev->right != cur){
+                    prev = prev->right;
+                }
+                
+                if(prev->right != cur){ //Cur is just visited once and from the top. Hence make link, print cur and visit left.
+                    prev->right = cur;
+                    ans.push_back(cur->val);
+                    cur = cur->left;
+                }else{ //Came here after visiting whole left subtree. Hence remove link.
+                    prev->right = NULL;
+                    cur = cur->right;
+                }
             }
         }
-        
         return ans;
     }
-    //T - O(n)
-    //S - O(h)  = O(n) in case of skewed trees.
+    //T - O(n) amortized
+    //S - O(1)
 };
