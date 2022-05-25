bb2aabdf43cfd9dc3ae94d47c2b4a7c0d36f7ca2
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-25 11:26:25 +0530

    Time: 4 ms (39.47%), Space: 8.4 MB (42.91%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,7 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/* Recursive
+/*
+ Recursive
 class Solution {
 public:
     void inorder(TreeNode *root, vector<int> &ans){
@@ -28,7 +29,7 @@ public:
     //T - O(n)
     //S - O(h) = O(n) in case of skewed trees
     
-};*/
+};
 
 //Iterative
 class Solution {
@@ -38,7 +39,7 @@ public:
         //Approach: Left Self Right (Use stack for simulating recursion)
         //Step-1. Use while loop and start from the root
         //Step-2. For each node push it into the stack and go to its left node.
-        //Step-3. If node == NULL, set node = pop from the stack (it will directly reach to the closest ancestor which is not printed). Then do similar steps on that node.
+        //Step-3. If node == NULL, set node = pop from the stack (it will directly reach to the closest     ancestor which is not printed). Then do similar steps on that node.
         //Step-4. Once stack is empty it means every node is printed along with its subtrees. hence break.
         
         vector<int> ans;
@@ -61,5 +62,45 @@ public:
     }
     //T - O(n)
     //S - O(h) = O(n) in case of skewed trees
-    
+    */
+
+class Solution {
+public:
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        //Step-1. While cur is not empty traverse. (Fact: Always leftmost node is visited first and rightmost node is visited last)
+        //Case-1: If(cur doesn't have left), Just print the cur and move to right child.
+        //Case-2: If(cur have left), Go to the rightmost node int the left subtree and if its right link is NULL(it means left subtree is not visited yet), make its right link to the cur and move to the left child.
+        //If its right link is pointing to the cur,(it means we came here after visiting left subtree), so just remove the link, print the cur and move to the right subtree.
+        
+        vector<int> ans;
+        TreeNode *cur = root;
+         
+        while(cur){
+            if(!cur->left){  //If cur doesn't have left, just print this and go to right then.
+                ans.push_back(cur->val);
+                cur = cur->right;
+            }else{
+                //Go to rightmost node in the leftsubtree and make it right link point to cur.
+                TreeNode *prev = cur->left;
+                while(prev->right != NULL && prev->right != cur){
+                    prev = prev->right;
+                }
+                
+                if(prev->right != cur){ //Cur is just visited once and from the top. Hence make link and visit left.
+                    prev->right = cur;
+                    cur = cur->left;
+                }else{ //Came here after visiting whole left subtree
+                    prev->right = NULL;
+                    ans.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+        
+        return ans;
+    }
+    //T - O(n)
+    //S - O(1)
+
 };
