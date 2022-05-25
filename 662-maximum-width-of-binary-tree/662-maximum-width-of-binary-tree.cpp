6a215bfa37c288dd08c5da089ade0ec244bd21e2
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-25 20:23:16 +0530

    Time: 11 ms (65.05%), Space: 17.4 MB (21.72%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -11,20 +11,25 @@
  */
 class Solution {
 public:
+    //Find width on the same level only
+    //Warning: Beware of Integer oveflow (testcases)
+    //For this used trick of starting every level with node number 0.(Refer to code) 2*(i-min_i) and 2*(i-min_i)+1 for childs
     int widthOfBinaryTree(TreeNode* root) {
+        
+        //Approach: Traverse the tree in the level order and for each level find width = lastNodeNo-firstNodeNo+1. Use 2*(i-min_i) and 2*(i-min_i)+1 for generating numbers for childs
         queue<pair<TreeNode*, long>> q;
         if(root != NULL) q.push({root,  0});
         int max_width = 0;
         int leftNodeNo = 0, rightNodeNo = 0;
         
+        //Way to travel the same level nodes only at a time
         while(!q.empty()){
             int size = q.size();
-            //cout << endl << endl <<"level "<< "  ";
-            for(int i=0; i<size; i++){
+         
+            for(int i=0; i<size; i++){ 
                 TreeNode *cur = q.front().first;
                 long nodeNo = q.front().second; q.pop();
-                //cout << nodeNo << " ";
-
+            
                 if(i==0) leftNodeNo = nodeNo;
                 if(i == size-1) rightNodeNo = nodeNo;
                 
@@ -38,4 +43,6 @@ public:
         
         return max_width;
     }
+    //T - O(n)
+    //S - O(1)
 };
