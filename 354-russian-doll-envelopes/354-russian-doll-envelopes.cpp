23cd5eae9444a64a2c85f25496a8927ba534d0ab
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-26 23:16:24 +0530

    Time: 401 ms (79.83%), Space: 77.5 MB (70.81%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,25 +1,30 @@
+
+
 class Solution {
 public:
-    
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-     
-      sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
-      });
-     
-      vector<int> dp;
-      for (auto envelope: envelopes) {
-        auto it = lower_bound(dp.begin(), dp.end(), envelope[1]);
-        if (it == dp.end()){
-         
-          dp.push_back(envelope[1]);
-        } else {
-       
-     
-          *it = envelope[1];
-        }
-      }
-     
-      return dp.size();
+    static bool compare(vector<int>&a , vector<int>& b)
+    {
+        if(a[0] == b[0]) return a[1] > b[1];
+        return a[0] < b[0];
     }
+
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        //Sort whole array with respect to first element in non-decreasing order and for same values of first elements sort second elements in decreasing order. So that i don't pick the elements with same width twice.
+        //Then apply LIS on second elements and get the ans as LIS.
+        sort(envelopes.begin(), envelopes.end(), compare);
+        
+        vector<int> temp;
+        temp.push_back(envelopes[0][1]);
+        for(int i=1; i<envelopes.size(); i++){
+            if(temp.back() < envelopes[i][1]) temp.push_back(envelopes[i][1]);
+            else{
+                int tempInd = lower_bound(temp.begin(), temp.end(), envelopes[i][1])-temp.begin();
+                temp[tempInd] = envelopes[i][1];
+            }
+        }
+        
+        return temp.size();
+    }
+    //T - O(nlogn) for sorting and binary searching on every index
+    //S - O(n)
 };
