aea2fb2a5396928b3b68976162d966a1e206e61a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-28 16:04:54 +0530

    Time: 4 ms (82.22%), Space: 16.7 MB (11.20%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -14,10 +14,13 @@ public:
     
     
     bool isSymmetric(TreeNode* root) {
-        
+        //Mirror-> left(right) subtree of every node of tree1(root->left) should mirror to right(left) subtree of corresponding node of tree2(root->right). which means nodes of both trees should coincide when visited in Mirror(opposite) order.
+        //Step-1. Traverse nodes of both the tree in opp order. if node1->val != node2->val || (both doesn't have same no. of NOT NULL connections). Then return false. 
+        //Step-2. After one tree is traversed fully, check if other is fully traversed or Not.
+        //Step-3. If not return false, else return true.
         queue<TreeNode*> q1, q2;
-        if(root->left) q1.push(root);
-        if(root->right) q2.push(root);
+        if(root && root->left) q1.push(root);
+        if(root && root->right) q2.push(root);
         
         while(!q1.empty() && !q2.empty()){
             TreeNode *cur1 = q1.front(); q1.pop();
@@ -35,4 +38,6 @@ public:
         if(!q1.empty()||!q2.empty()) return false;
         return true;
     }
+    //T - O(n)
+    //S - O(n) (2 stack with total just n space)
 };
