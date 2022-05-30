a88a66deae77ba71d2ba066823166166a7081390
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-30 12:47:22 +0530

    Time: 129 ms (20.27%), Space: 35.1 MB (13.82%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    
+    
+    int dfs(vector<vector<int>> &matrix, int i, int j, vector<vector<int>> &dp, int n, int m, int lastVal){
+        
+        if(i==-1 || i==n || j==-1 || j==m || matrix[i][j] <=lastVal) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        vector<int> dx({-1, +1, 0, 0});
+        vector<int> dy({0, 0, -1, +1});
+        int ans = 0;
+        for(int d = 0; d < 4; d++){
+            int ni = i+dx[d], nj = j+dy[d];
+            ans = max(ans, 1+dfs(matrix, ni, nj, dp, n, m, matrix[i][j]));
+        }
+        
+        return dp[i][j]=ans;       
+    }
+    
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+       
+        //This problem has an optimal substructure and There are overlapping calls on the same coordinates
+        //So, we use dp and then total call on any coordinate points will max be 1.
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        int ans = 0;
+        for(int i =0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                ans = max(ans, dfs(matrix, i, j, dp, n, m, -1));
+            }
+        }
+        
+       return ans;
+    }
+    //T - O(n*m)
+    //S - O(n*m)
+};
