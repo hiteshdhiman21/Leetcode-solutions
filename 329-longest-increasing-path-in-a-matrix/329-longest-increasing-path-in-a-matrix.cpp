fd6ff5e1b0225cf309063d64cd7d3616d85e1bf0
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-05-30 12:52:12 +0530

    Time: 193 ms (16.07%), Space: 34.8 MB (13.89%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -5,7 +5,7 @@ public:
     
     int dfs(vector<vector<int>> &matrix, int i, int j, vector<vector<int>> &dp, int n, int m, int lastVal){
         
-        if(i==-1 || i==n || j==-1 || j==m || matrix[i][j] <=lastVal) return 0;
+        if(i==-1 || i==n || j==-1 || j==m || matrix[i][j] <= lastVal) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
         
@@ -24,6 +24,10 @@ public:
        
         //This problem has an optimal substructure and There are overlapping calls on the same coordinates
         //So, we use dp and then total call on any coordinate points will max be 1.
+        //Step-1. We try to calculate ans using dfs on every possible coordinate and therefore try to go to max depth.
+        //Step-2. On each coordinate, if the ans is not calculated before we go to its neighbours and calculate the maximum ans and store in dp and return. Otherwise, we just return the ans.
+        //Step-3. So, any neighbours will be visited at most once. Now return the max ans out of all starting coordinates.
+        
         int n = matrix.size(), m = matrix[0].size();
         vector<vector<int>> dp(n, vector<int>(m, -1));
         
