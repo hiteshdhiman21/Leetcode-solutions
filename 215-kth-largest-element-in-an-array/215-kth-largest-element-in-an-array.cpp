d8d45c08e17dd1641f418faa89bb725d887f72c8
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-03 19:02:02 +0530

    Time: 162 ms (7.17%), Space: 10 MB (61.36%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
+    /*int findKthLargest(vector<int>& nums, int k) {
         //Step-1. Maintain a min heap so that we can remove the minimum element easily to get a heap of k largest elements.
         //Step-2. Iterate over nums array & While min heap size < k, just push the element into the min heap.
         //Step-3. If min heap size == k and also min heap's min element < x, remove min element and insert x into the heap.
@@ -18,5 +18,36 @@ public:
         return mn_heap.top();
     }
     //T - O(nlogk)
-    //S - O(k)
+    //S - O(k)*/
+    
+    int partition(vector<int>& nums, int left, int right){
+        int l = left+1, r = right;
+        while(l<=r){
+            if(nums[l] <= nums[left]) l++;
+            else if(nums[r] >= nums[left]) r--;
+            else swap(nums[l++], nums[r--]);
+        }
+        
+        swap(nums[l-1], nums[left]);
+        return l-1;
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size()-1;
+        //kth largest would be at index nums.size()-k in sorted array
+        int kthInd = nums.size()-k;
+        
+        while(true){
+            int idx = partition(nums, left, right);
+            
+            if(idx < kthInd){
+                left = idx+1;
+            }else if(idx > kthInd){
+                right = idx-1;
+            }else{
+                return nums[idx];
+            }      
+        }
+        return -1;
+    }
 };
