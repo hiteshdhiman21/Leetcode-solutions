6339fa670ebd4d1999eb0e329f6493e02e390722
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-03 18:03:20 +0530

    Time: 8 ms (80.86%), Space: 10.2 MB (21.27%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        //Step-1. Maintain a min heap so that we can remove the minimum element easily to get a heap of k largest elements.
+        //Step-2. Iterate over nums array & While min heap size < k, just push the element into the min heap.
+        //Step-3. If min heap size == k and also min heap's min element < x, remove min element and insert x into the heap.
+        //Step-4. At last min heap will be of  k size. Just return the minimum of those largest k elements.
         priority_queue<int, vector<int>, greater<int>> mn_heap;
         for(int x: nums){
             if(mn_heap.size() < k){
