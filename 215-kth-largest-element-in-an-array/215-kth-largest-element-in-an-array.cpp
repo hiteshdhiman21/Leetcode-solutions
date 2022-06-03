1385c196016552633543521e734b016862bbe067
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-03 19:19:59 +0530

    Time: 195 ms (5.18%), Space: 9.9 MB (97.37%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -20,7 +20,7 @@ public:
     //T - O(nlogk)
     //S - O(k)*/
     
-    int partition(vector<int>& nums, int left, int right){
+    int partition(vector<int>& nums, int& left, int& right){
         int l = left+1, r = right;
         while(l<=r){
             if(nums[l] <= nums[left]) l++;
@@ -33,13 +33,18 @@ public:
     }
     
     int findKthLargest(vector<int>& nums, int k) {
+        //Approach: Quick select (just like partition of quick sort)
+        //partition() returns the partition index along with the max value of left half present at the partition index. All the elements at indexes nums[1...partition] < nums[partition+1 ... n]
+        //If parIdx < kth value Indes, we have to call partition on (parIdx+1, right)
+        //Else if parIdx > kth value index, we have to call partition on (left, parIdx-1)
+        //Else parIdx == kth value index, and as the smallest parIdx+1 values are present at first [0..parIdx] and the largest of them is present at [parIdx], and we need kth[parIdxth or largest value of first k+1 values in sorted array] value of the sprted array. Return nums[parIdxth].
         int left = 0, right = nums.size()-1;
         //kth largest would be at index nums.size()-k in sorted array
+        //It means we need the kth value in sorted array or largest of the first k+1 values
         int kthInd = nums.size()-k;
         
         while(true){
             int idx = partition(nums, left, right);
-            
             if(idx < kthInd){
                 left = idx+1;
             }else if(idx > kthInd){
@@ -50,4 +55,6 @@ public:
         }
         return -1;
     }
+    //T - Average(n) , Worst(n^2)
+    //S - O(1)
 };
