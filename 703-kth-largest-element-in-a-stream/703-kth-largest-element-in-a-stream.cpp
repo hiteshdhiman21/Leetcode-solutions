074159274c8545692ae0cfd71519053a55e2aab1
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-03 16:09:03 +0530

    Time: 35 ms (84.86%), Space: 19.9 MB (18.67%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,5 +1,6 @@
 class KthLargest {
 private:
+    //Heap to store k largest elements.
     priority_queue<int, vector<int>, greater<int>> mn_heap;
     int k;
     
@@ -10,6 +11,11 @@ public:
     }
     
     int add(int val) {
+        //Assumption: Mn_heap will only store k largest elements of all the elements.
+        //Step-1. If there are less than k elements. just push the element into the heap.
+        //Step-2. Else if(mn_heap.top() < val), This val will never be kth largest element, as now there are k or more elements larger than it already present. Else, remove the smallest element(as now it is not in k largest elements) of these k larger elements and insert val.
+        //Step-3. Return the minimum of k largest element.
+        
         if(mn_heap.size() < k) mn_heap.push(val);
         else{
             if(mn_heap.top() < val){
@@ -20,6 +26,8 @@ public:
         
         return mn_heap.top();
     }
+    //T - O(nlogk)
+    //S - O(k)
 };
 
 /**
