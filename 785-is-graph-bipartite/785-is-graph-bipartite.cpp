8a570b8a309904762daedc87c9fbc41239c5ee1d
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-06 16:41:50 +0530

    Time: 55 ms (11.75%), Space: 13.8 MB (26.62%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     
-    bool bipartiteBFS(int st, vector<vector<int>>& graph, vector<int> &part){
+    bool bipartiteBFS(int st, vector<vector<int>>& graph, vector<int> &color){
+        //Step-1. Color the first vertex as 0.
+        //Step-2. For every node, color all of its uncolored negihbours with oppsoite colors.
+        //Step-3. If any neighbour is colored with the same color as the current node. Return false.
+        //Steo-4. At last return true.
+        
         queue<int> q;
         q.push(st);
-        part[st] = 1;
+        color[st] = 0;
         
         while(!q.empty()){
             int cur = q.front(); q.pop();
-            int curPart = part[cur];
+            int curPart = color[cur];
             
             for(int x:graph[cur]){
-                if(part[x] == 0){
+                if(color[x] == -1){
                     q.push(x);
-                    part[x] = 3-curPart; //For 1, it is 2 and for 2, it is 3
-                }else if(part[x] != 3-curPart){
+                    color[x] = 1-curPart; //For 1, it is 0 and for 0, it is 1
+                }else if(color[x] != 1-curPart){
                     return false;
                 }
             }
@@ -23,12 +28,17 @@ public:
     }
     
     bool isBipartite(vector<vector<int>>& graph) {
+        //Approach: It is just the same as dividing the graph into 2 parts or 2-coloring problem.
+        //If i am able to divide the graph into two parts or color it using 2 color then return true. Else return false.
+        //Step. Call isBipartite for all components and if any return false, return false. Else return true.
+        
+        
         int n = graph.size();
         
-        vector<int> part(n, 0);
+        vector<int> color(n, -1);
         
-        for(int i =0; i<n; i++){
-            if(!part[i] && !bipartiteBFS(i, graph, part)) return false;
+        for(int i =0; i<n; i++){ 
+            if(color[i] == -1 && !bipartiteBFS(i, graph, color)) return false;
         }
         
         return true;
