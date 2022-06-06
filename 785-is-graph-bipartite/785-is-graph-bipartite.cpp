2bbe02d45f1c939b5a7c7341ad3d0593c248c778
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-06 16:50:07 +0530

    Time: 68 ms (6.80%), Space: 13.5 MB (54.50%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     
-    bool bipartiteBFS(int st, vector<vector<int>>& graph, vector<int> &color){
-        //Step-1. Color the first vertex as 0.
-        //Step-2. For every node, color all of its uncolored negihbours with oppsoite colors.
+    bool bipartiteDFS(int st, vector<vector<int>>& graph, vector<int> &color, int myColor){
+        //Step-1. Color the current vertex as given color.
+        //Step-2. Color all of its uncolored negihbours with oppsoite colors.
         //Step-3. If any neighbour is colored with the same color as the current node. Return false.
         //Steo-4. At last return true.
         
-        queue<int> q;
-        q.push(st);
-        color[st] = 0;
+        color[st] = myColor;
         
-        while(!q.empty()){
-            int cur = q.front(); q.pop();
-            int curPart = color[cur];
-            
-            for(int x:graph[cur]){
-                if(color[x] == -1){
-                    q.push(x);
-                    color[x] = 1-curPart; //For 1, it is 0 and for 0, it is 1
-                }else if(color[x] != 1-curPart){
-                    return false;
-                }
-            }
+        for(int x: graph[st]){
+            if(color[x] == -1 && !bipartiteDFS(x, graph, color, 1-myColor)) return false;
+            else if(color[x] != 1-myColor) return false;
         }
+        
         return true;
     }
     
     bool isBipartite(vector<vector<int>>& graph) {
         //Approach: It is just the same as dividing the graph into 2 parts or 2-coloring problem.
         //If i am able to divide the graph into two parts or color it using 2 color then return true. Else return false.
-        //Step. Call isBipartite for all components and if any return false, return false. Else return true.
+        //Step. Call bipartiteDFS for all components and if any return false, return false. Else return true.
         
         
         int n = graph.size();
@@ -38,7 +28,7 @@ public:
         vector<int> color(n, -1);
         
         for(int i =0; i<n; i++){ 
-            if(color[i] == -1 && !bipartiteBFS(i, graph, color)) return false;
+            if(color[i] == -1 && !bipartiteDFS(i, graph, color, 0)) return false;
         }
         
         return true;
