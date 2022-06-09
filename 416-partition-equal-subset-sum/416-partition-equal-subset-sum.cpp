415b7997d40e5ac06389e0e01f0fe56506e07fff
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-09 22:43:30 +0530

    Time: 489 ms (17.49%), Space: 274 MB (5.02%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    bool canFindSum(vector<int>& nums, int ind, int req, vector<vector<int>>& dp){
+        if(ind == nums.size()){
+            return req==0;
+        }
+        
+        if(dp[ind][req] != -1) return dp[ind][req];
+        
+        bool res = canFindSum(nums, ind+1, req, dp);
+        if(req >= nums[ind]) res = res|canFindSum(nums, ind+1, req-nums[ind], dp);
+        
+        return dp[ind][req] = res;
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int x:nums) sum+= x;
+        
+        if(sum%2 == 1) return false;
+        
+        vector<vector<int>> dp(nums.size()+1, vector<int>(nums.size()*100+1, -1));
+        
+        return canFindSum(nums, 0, sum/2, dp);
+    }
+    //T - O(n*TotalSum)
+    //S - O()
+};
