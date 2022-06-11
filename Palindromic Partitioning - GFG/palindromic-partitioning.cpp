46e166e9896f1bf57e4f71aaf8634db7fce6b934
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-11 11:52:48 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -19,6 +19,11 @@ public:
 
     int palindromicPartition(string str)
     {
+      /*Step-1. Set dp[n] = 0. As string of length 0 have 0 partitions.
+        Step-2. For all i starting from the last Index, try to check for all palindromic substrings(ending at j). Set dp[i] = min(dp[i], 1+dp[j+1])
+                '1' added because we are partitioning the string at index j.
+        Step-3. Finally return dp[0]-1. because all dp[ind] stores an additional count for partition line after the last character which is separating
+                substring before from the empty string. storage is like this ababa|cfgfc|lol| but the last line is not needed.*/ 
         int n = str.size();
         
         vector<int> dp(n+1, n);
@@ -34,6 +39,8 @@ public:
         
         return dp[0]-1;
     }
+    //T - O(n^2)
+    //S - O(n)
 };
 
 // { Driver Code Starts.
