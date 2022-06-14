3e440ca0cee722f6e766c0f4ac35e4746864d8ea
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-14 12:23:29 +0530

    Time: 31 ms (61.01%), Space: 19.4 MB (14.16%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int ind = -1;
+        
+      /*Step-1. Maintain ind such that all the elements placed till ind are already non-zeros.
+        Step-2. Iterating over the array and if nums[i] == 0, skip it
+        Step-3. Else nums[++ind] = nums[i].*/
+        
+        int ind = -1; 
         for(int i =0; i< nums.size(); i++){
             
            if(nums[i] !=0){
