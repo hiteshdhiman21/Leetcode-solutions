00807205a36ee08bcba6fb4396e33bb89c02e25a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-16 23:10:32 +0530

    Time: 23 ms (30.57%), Space: 12.8 MB (16.59%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,25 +1,35 @@
+class comp{
+    public:
+    bool operator()(pair<string, int> f, pair<string, int> s){
+        if(f.second < s.second) return true;
+        else if(f.second > s.second) return false;
+        else return f.first > s.first;
+    }
+};
+
 class Solution {
 public:
+    
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> freq;
         for(auto& str:words)
             freq[str]++;
         
-        vector<vector<string>> freqBucket(words.size()+1);
+        priority_queue<pair<string, int>, vector<pair<string, int>>, comp> pq; 
         for(auto &__:freq){
-            freqBucket[__.second].push_back(__.first);
+            pq.push(__);
         }
         
-        vector<string> ans;
-        for(int f = words.size(); f>=1 && ans.size() < k; f--){
-            sort(freqBucket[f].begin(), freqBucket[f].end());
-            for(auto &str: freqBucket[f]){
-               ans.push_back(str);
-                if(ans.size() == k) break;
-            }
-                
+        vector<string> res;
+        while(!pq.empty()){
+            res.push_back(pq.top().first);
+            pq.pop();
+            
+            if(res.size() == k)
+                break;
         }
-        return ans;
+        
+        return res;
     }
     //T - O(nlogk)
     //S - O(n)
