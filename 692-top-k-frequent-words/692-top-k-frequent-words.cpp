909bbc21af3cda640341a26bb43d4755be5bf1f9
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-17 10:16:04 +0530

    Time: 20 ms (44.35%), Space: 12.4 MB (95.86%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,9 +1,11 @@
 class comp{
     public:
     bool operator()(pair<string, int> f, pair<string, int> s){
-        if(f.second < s.second) return true;
-        else if(f.second > s.second) return false;
-        else return f.first > s.first;
+        //For min heap second have to be min
+        if(f.second > s.second) return true;
+        else if(f.second < s.second) return false;
+        else return f.first < s.first;
+        //Min freq will be at top and for same freq lexiographic bigger will be at top
     }
 };
 
@@ -17,16 +19,18 @@ public:
         
         priority_queue<pair<string, int>, vector<pair<string, int>>, comp> pq; 
         for(auto &__:freq){
+            if(pq.size() < k)
             pq.push(__);
+            else if(__.second > pq.top().second || (__.second == pq.top().second && __.first < pq.top().first)){
+                pq.pop();
+                pq.push(__);
+            }
         }
         
-        vector<string> res;
+        vector<string> res(k);
         while(!pq.empty()){
-            res.push_back(pq.top().first);
+            res[--k] = pq.top().first;
             pq.pop();
-            
-            if(res.size() == k)
-                break;
         }
         
         return res;
