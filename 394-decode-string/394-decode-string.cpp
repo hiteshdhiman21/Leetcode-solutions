9e8f3883323733cda3e65b2533c57fa427761138
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-17 16:55:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (96.75%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -7,7 +7,7 @@ public:
         Step-1. Iterate over all the characters ch of the string
         Step-2. If ch != ']', just push it into the stack
         Step-3. Else close bracket found and Hence we can decode this substring. Pop all the characters of the substring inside the corresponding [] brackets and also pop out the k before the bracker. Since the string will be popped in FILO order and we are adding in same order, There reverse the string. Since (int)k is also in reverse string format, so reverse it and then convert it into integer format.
-        Step-4. Now after conversions, push the whole string k times into the stack
+        Step-4. Now after conversions, push the whole string k times into the stack (means decoded string).
         Step-5. Finally the whole string will be present in the stack so copy it in a string format and reverse it.
         Step-6. Finally return the string*/
         
@@ -18,13 +18,13 @@ public:
             }else{
                 
                 string s;
-                while(st.top() != '['){
+                while(st.top() != '['){ //Popping the string
                     s += st.top(); st.pop();
                 }
                 
                 st.pop(); //Popping '['
                 
-                string kStr;
+                string kStr; //Popping k
                 while(!st.empty() && isdigit(st.top())){
                     kStr += st.top(); st.pop();
                 }
@@ -34,6 +34,7 @@ public:
                 
                 int k = stoi(kStr);
                 
+                //Finally inserting the substring after decoding. (Inserting same string k times)
                 while(k--){
                     for(char ch: s)
                         st.push(ch);
