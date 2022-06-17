5d3d904504c9b19f5df9fe7247bbd190e3324a0a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-17 13:32:35 +0530

    Time: 51 ms (29.25%), Space: 25.1 MB (53.49%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+      /*Step-1. Reverse the whole array. Last k elements now will be in first k positions and first n-k will now be at the last n-k positions (both in reverse order).
+        Step-2. Now just reverse the first k and last n-k elements separately and we will get the required array*/
+      
         k = k%nums.size();
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin()+k);
