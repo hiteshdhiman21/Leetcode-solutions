4444e550d52bd555971bd741b66fb35cad7a93dd
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-17 23:03:56 +0530

    Time: 112 ms (34.01%), Space: 31.2 MB (75.92%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -7,23 +7,17 @@ public:
         Step-2. Copy k elements from that point into the res array and return it*/
         int n = nums.size();
         int start = 0, end = nums.size()-k;
-        int toBeginFrom = -1;
-        
-        if(start == end)
-            toBeginFrom = start;
         
         while(start < end){
             int mid = start+(end-start)/2;
             if(mid+k < n && x-nums[mid] > nums[mid+k]-x){ //mid+1 is better option than mid
-                toBeginFrom = mid+1;
                 start = mid+1;
             }else{ //mid is better option than mid+1
-                toBeginFrom = mid;
                 end = mid;
             }
         }
         
-        vector<int> res(nums.begin()+toBeginFrom, nums.begin()+toBeginFrom+k);
+        vector<int> res(nums.begin()+start, nums.begin()+start+k);
         return res;
     }
     
