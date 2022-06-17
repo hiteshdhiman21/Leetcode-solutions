ead31ecf66ced6bc7eaa4d0615cb424b11ec13ec
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-17 16:54:06 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (98.96%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
         
-    string decodeString(string str) {
+    string decodeString(string str){
+        
+      /*Problem involve subproblems so intuition say use RECURSION or STACK
+        Step-1. Iterate over all the characters ch of the string
+        Step-2. If ch != ']', just push it into the stack
+        Step-3. Else close bracket found and Hence we can decode this substring. Pop all the characters of the substring inside the corresponding [] brackets and also pop out the k before the bracker. Since the string will be popped in FILO order and we are adding in same order, There reverse the string. Since (int)k is also in reverse string format, so reverse it and then convert it into integer format.
+        Step-4. Now after conversions, push the whole string k times into the stack
+        Step-5. Finally the whole string will be present in the stack so copy it in a string format and reverse it.
+        Step-6. Finally return the string*/
         
         stack<char> st;
         for(char ch: str){
@@ -43,7 +51,6 @@ public:
         reverse(res.begin(), res.end());
         return res;
     }
-    
     //T - O(length of Decoded string)
     //S - O(length of Decoded string)
 };
