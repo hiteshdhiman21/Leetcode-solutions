6288cc915e78013ec9e5065e90701f6f543fdfb0
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-18 12:27:34 +0530

    Time: 18 ms (53.83%), Space: 7.1 MB (54.42%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+        int mostFrequent = 0;
         int maxLength = 0;
-        for(char ch = 'A'; ch <= 'Z'; ch++){
-            deque<int> dq;
-            int start = 0;
-            for(int i = 0; i < s.size(); i++){
-                if(s[i] != ch){
-                    dq.push_back(i);
-                }
-                
-                if(dq.size()>k){
-                    start = dq.front()+1;
-                    dq.pop_front();
-                }
-                
-                maxLength = max(maxLength, i-start+1);
+        int left = 0;
+        
+        for(int right = 0; right < s.size(); right++){
+            freq[s[right]-'A']++;
+            mostFrequent = max(mostFrequent, freq[s[right]-'A']);
+            if(right-left+1-mostFrequent > k){
+                freq[s[left]-'A']--;
+                left++;
             }
             
+           // cout << left << " " << right << " " << mostFrequent << endl;
+            
+            maxLength = max(maxLength, right-left+1);
         }
         
         return maxLength;
