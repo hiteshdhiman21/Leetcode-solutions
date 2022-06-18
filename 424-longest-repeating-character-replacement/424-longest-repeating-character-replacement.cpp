4b06b47714fe48cd17f613f5e75b33fae06396cb
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-18 12:40:24 +0530

    Time: 10 ms (82.27%), Space: 7 MB (54.42%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        
+      /*Intuition: Originally Use sliding window for all 26 different character. But we can also make use of mostFrequent frequency and some facts about how the algorithm will work.
+      Fact 1: For substring to be valid, substring len - most Frequent charcter <= k. (i.e, the min characters to make string valid <= k).
+      Fact 2: Higher the most frequent character, Higher the valid substring length.
+      Fact 3: If we have calculated fully(i.e len - most Frequent charcter > k condition fails and then we increment left) maxLength for some mostFrequent frequency, then in future the maxLength for same mostFrequentCharater <= maxLength already calculated.
+      Fact 4: If in future, the mostFrequent frequency decrease in relatity but we don't do that in algorithm. Then no need to worry as problem is asking for maxLength and decreased mostFrequent frequency will give some less length now onward but the same mostFrequent frequency will give the same or <= maxLength calculated and in Both the cases(reality or algorithm), the ans = maxLength caclulated.
+      Fact 5: If in future, the mostFrequent frequency increases in reality then we will also increase it in algorithm and Therefore it would result into greater maxLength in that case only.
+      Fact 6: Hence we only need to consider changing(only increasing if it is high in current substring as decreasing and remain same will not change ans) mostFrequency while incrementing right and we not need to decrease while decrementing left pointer
+      */
+        
         vector<int> freq(26, 0);
         int mostFrequent = 0;
         int maxLength = 0;
@@ -14,13 +24,11 @@ public:
                 left++;
             }
             
-           // cout << left << " " << right << " " << mostFrequent << endl;
-            
             maxLength = max(maxLength, right-left+1);
         }
         
         return maxLength;
     }
-    //T - O(26n)
-    //S - O(k)
+    //T - O(n)   
+    //S - O(26)
 };
