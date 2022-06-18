60bea6aec7ca79f5f76de42e732e5328a07eeb7e
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-18 23:44:09 +0530

    Time: 123 ms (26.04%), Space: 16.4 MB (96.12%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int> dp(m, 0);
         
         int maxArea = 0;
-        
-        for(int i=0; i<n; i++){
-            dp[i][0] = matrix[i][0]-'0';
-            maxArea = max(maxArea, dp[i][0]);
-        }
             
-        for(int j=1; j<m; j++){
-            dp[0][j] = matrix[0][j]-'0';
-            maxArea = max(maxArea, dp[0][j]);
+        for(int j=0; j<m; j++){
+            dp[j] = matrix[0][j]-'0';
+            maxArea = max(maxArea, dp[j]*dp[j]);
         }
         
         for(int i=1; i<n; i++){
+            int dpI_1AndJ_1 = dp[0];
+            dp[0] = matrix[i][0]-'0';
+            maxArea = max(maxArea, dp[0]*dp[0]);
+            
             for(int j=1; j<m; j++){
-                if(matrix[i][j] == '0') continue;
                 
-                dp[i][j] = min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]))+1;
-                maxArea = max(maxArea, dp[i][j]*dp[i][j]);
+                
+                int dpI_1AndJ = dp[j];
+                if(matrix[i][j] != '0')
+                    dp[j] = min(dpI_1AndJ, min(dp[j-1], dpI_1AndJ_1))+1;
+                else
+                    dp[j] = 0;
+                dpI_1AndJ_1 = dpI_1AndJ;
+                maxArea = max(maxArea, dp[j]*dp[j]);
             }
         }
            
