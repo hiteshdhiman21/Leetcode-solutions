4de64c416ada2f201d61a67ed70e48716daef856
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-21 01:12:27 +0530

    Time: 323 ms (8.32%), Space: 49.6 MB (13.93%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -11,6 +11,13 @@ public:
       /*Approach: BellMann Ford algo
         Relax all the edges k+1 times.
         In ith iteration path of dist <=i from source will get optimize fully.
+        Modification: We will calculate ith level distances using (<= i-1)th level distances
+        i.e. We will use prevDist[] = dist[] vector for distances calculated using i-1 stops from source and now we will calculate dist[] using prevDist involving i-1 stops and introducing a new stop.
+        Step-1. Iterate for k+1 times to include/not include k stops in between.
+        Step-2. Copy prev level dist vector in prevDist vector.
+        Step-3. Now using that prevDist vector try to update the originalDist vector(This will ensure that only ith level distances are updated at ith iteration if it is possible and it is minimum)
+        Step-4. Finally return the ith Dist vector
+        Testcase: 9, [[0,1,1],[1,2,1],[2,3,1],[3,7,1],[0,4,3],[4,5,3],[5,7,3],[0,6,5],[6,7,100],[7,8,1]], 0, 8, 3
         */
         
         vector<int> dist(n, INT_MAX);
