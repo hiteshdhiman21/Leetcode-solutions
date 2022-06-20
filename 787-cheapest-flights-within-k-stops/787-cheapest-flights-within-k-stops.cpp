8f5acfb67c1821ef71ae856e2b9bce2ffe41f42b
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-21 00:02:01 +0530

    Time: 35 ms (75.35%), Space: 13.3 MB (68.63%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -4,6 +4,13 @@ public:
     using MinHeap = priority_queue< tuple<int,int,int>, vector<tuple<int, int, int>>, greater<tuple<int,int,int>> >; 
     
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+      /*Approach: Dijkstra algo with some modifications
+        In addition to {dist, node} also use {dist, node, stops}
+        Use stops and dist array for setting a benchMark. dist[x] stores the minimum dist till now to reach x from source and stops[x] stores the no. of stops to reach x via minPath. 
+        i.e. If a new path comes with dist > preMinDist and stops > preStops[for minDist]. Then no need to choose this path.
+        Else if a new path comes with dist < preMinDist (stops not matter). Then this path can be potential ans. hence push it in the queue and update dist[x] and stops[x] for this path.
+        Else if a new path comes with dist > preMinDist but stops < preStops. Then this path can be potential ans. hence putsh it in the queue but dont update dist[x] and stops[x] for this path*/
+        
         
         vector<vector<pair<int,int>>> adj(n);
         for(int i = 0; i < flights.size(); i++)
@@ -20,8 +27,7 @@ public:
         
         while(!pq.empty()){
             auto [curDist, cur, curStops] = pq.top(); pq.pop();
-            
-            cout << endl << curDist << " " << cur << endl;            
+                                                                   
             if(cur == dst) return curDist;
             if(curStops == k+1) continue;
             for(auto& [x, curToxDist]: adj[cur]){
@@ -30,9 +36,11 @@ public:
                 
                 if(nextDist < dist[x]){
                     dist[x] = nextDist;
-                    stops[x] = nextStops;
+                    stops[x] = nextStops; 
                     pq.push({nextDist, x, nextStops});
                 }else if(nextStops < stops[x]){
+                    dist[x] = nextDist;
+                    stops[x] = nextStops; 
                     pq.push({nextDist, x, nextStops});
                 }
             }
