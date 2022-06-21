54e2b87fdea01c6201d4732df5af34001915afae
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-21 20:58:15 +0530

    Time: 16 ms (45.10%), Space: 8.6 MB (44.20%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int calculate(string s) {
+      /*Stack - No BODMAS. Just perform Multiplication and division first according to their occurences at last perform addition and subtraction.
+        Storing the sign before the curNum in curOp
+        Step-1. Iterate over all the characters in s.
+        Step-2. If character is whitespace, just continue.
+        Step-3. If character is digit, Just add the digit into curNum.
+        Step-4. If it is the end of string or charater is some operator. Just perform the operation accoring to curOp.
+                If curOp == '+' or '-' just push the curNum with the appropriate sign into the stack(we will perform these operations later). Else if curOp == '*' or '/' Just perform the operation between the st.top() and curNum and push it into the stack after popping the first operand from stack. And for making the next number update the curNum = 0 and curOp = current character sign.*/
+        
+        
         int curNum = 0;
         int sign = 1;
         stack<int> st;
         
-        char op = '+';
+        char curOp = '+';
         
         for(int i = 0; i <= s.size(); i++){
             
@@ -16,9 +25,9 @@ public:
                 continue;
             }
                
-            if(op == '+') st.push(curNum);
-            else if(op == '-') st.push(-curNum);
-            else if(op == '*'){
+            if(curOp == '+') st.push(curNum);
+            else if(curOp == '-') st.push(-curNum);
+            else if(curOp == '*'){
                 int firstNum = st.top(); st.pop();
                 st.push(firstNum * curNum);
             }else{
@@ -29,10 +38,10 @@ public:
             curNum = 0;
                
             if(i==s.size()) continue;
-            else if(s[i] == '+') op = '+';
-            else if(s[i] == '-') op = '-';
-            else if(s[i] == '*') op = '*';
-            else op = '/';
+            else if(s[i] == '+') curOp = '+';
+            else if(s[i] == '-') curOp = '-';
+            else if(s[i] == '*') curOp = '*';
+            else curOp = '/';
         }
         
         int ans = 0;
