97ceddfae63f83a313a18590313386a3154bcd3a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-22 11:54:17 +0530

    Time: 430 ms (26.01%), Space: 101.2 MB (30.45%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -1,27 +1,26 @@
 class FreqStack {
     
-    vector<vector<int>> freqBuck;
+    map<int, stack<int>> freqBuck;
     unordered_map<int, int> freq;
     int maxFreq = 0;
     
 public:
     FreqStack() {
-        freqBuck.resize(1e5);
     }
     
     void push(int val) {
-        freq[val]++;
-        freqBuck[freq[val]].push_back(val);
+        freqBuck[++freq[val]].push(val);
         maxFreq = max(maxFreq, freq[val]);
     }
     
     int pop() {
-        int val = freqBuck[maxFreq].back();
-        freqBuck[maxFreq].pop_back();
-        if(freqBuck[maxFreq].size() == 0)
-            maxFreq--;
+        int val = freqBuck[maxFreq].top();
+        freqBuck[maxFreq].pop();
         freq[val]--;
-    
+        
+        if(freqBuck[maxFreq].empty())
+            maxFreq--;
+        
         return val;
     }
     //T - O(1) for each operation
