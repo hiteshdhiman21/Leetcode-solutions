208ab639c75bb7e8b4d48b69ffd9108f01c4cb4e
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-22 12:02:16 +0530

    Time: 15 ms (47.95%), Space: 8.1 MB (72.05%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.cpp
+++ 224-basic-calculator/224-basic-calculator.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int calculate(string s) {
-        int curRes = 0;
+        
+      /*Push in order [prevLevelResult, Whole sign of this level]  so that when this level end and we revert back to prevLevel(outer level), we can do prevLevelResult(outer level = Now curLevel) = prevLevelResult + sign*curLevelResult(inner level = Now prevLevel)*/
+         
+        int curRes = 0; //Maintains res of current bracket level
         int curNum = 0;
         int sign = 1;
         
         stack<int> st;
         for(int i = 0; i <= s.size(); i++){
-            
             
             if(i==s.size()){
                 curRes += sign*curNum;
@@ -25,12 +27,12 @@ public:
                 curNum = 0;
                 if(s[i] == '+') sign = 1;
                 else sign = -1;
-            }else if(s[i] == '('){
+            }else if(s[i] == '('){ //Start a new bracket level
                 st.push(curRes);
                 st.push(sign);
                 sign = 1;
                 curRes = 0;
-            }else if(s[i] == ')'){
+            }else if(s[i] == ')'){ //End the current bracket level and restart the old
                 curRes += sign*curNum;
                 curRes = st.top()*curRes; st.pop();
                 curRes = st.top() + curRes; st.pop(); 
