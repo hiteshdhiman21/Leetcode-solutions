e7057514c9b560b239ec4c09d707ded91e222ab9
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-23 14:53:50 +0530

    Added solution - LeetHub

--- Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
@@ -19,6 +19,12 @@ class Solution
     
         vector <int> search(string pat, string txt)
         {
+            //Rabin- karp Rolling Hash 
+          /*Step-1. First develop winHash and patHash
+            Step-2. Now start iterating over text and check if patHash == winHash. If true, then only check if the current substring and pattern are equal or not.
+                    If equal, then push the index into res vector.
+            Step-3. Finally update winHash for the next txt substring in O(1) time.
+            */
             long patHash = 0, winHash = 0;
             long wt = 1;
             
@@ -54,7 +60,8 @@ class Solution
             return res;
             
         }
-        /
+        //T - O(n*m)   //Avg case is far less O(n) < Tavg < O(n*m)
+        //S - O(1)
      
 };
 
