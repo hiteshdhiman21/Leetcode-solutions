edc889af74b30c5940008fe7cecc15600664bda2
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-23 15:19:51 +0530

    Added solution - LeetHub

--- Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
@@ -8,6 +8,7 @@ class Solution
 {
     int mod = 1e9+7;
     int base = 256;
+    
     public:
         bool isEqual(string &txt, int &si, string &pat){
             for(int i = 0; i<pat.size(); i++){
@@ -19,10 +20,11 @@ class Solution
     
         vector <int> search(string pat, string txt)
         {
-            //Rabin- karp Rolling Hash 
+            //Rabin- karp 
+            //Rolling hash type: str = "abyz", a*pow(256,3) + b*pow(256,2) + y*pow(256,1)+ z*pow(256, 0) with appropriate mod in between.
           /*Step-1. First develop winHash and patHash
-            Step-2. Now start iterating over text and check if patHash == winHash. If true, then only check if the current substring and pattern are equal or not.
-                    If equal, then push the index into res vector.
+            Step-2. Now start iterating over text and check if patHash == winHash. If true, then only check if the current substring and pattern are equal 
+                    or not. If equal, then push the index into res vector.
             Step-3. Finally update winHash for the next txt substring in O(1) time.
             */
             long patHash = 0, winHash = 0;
@@ -30,29 +32,25 @@ class Solution
             
             int len = pat.size();
             for(int i = pat.size()-1; i>=0; i--){
-                int charVal = int(pat[i]);
-                patHash = (patHash+(charVal*wt)%mod)%mod;
-                
-                charVal = int(txt[i]);
-                winHash = (winHash+(charVal*wt)%mod)%mod;
+               
+                patHash += pat[i]*wt; patHash %= mod;
+                winHash += txt[i]*wt; winHash %= mod;
                 
                 if(i!=0)
                 wt = (wt*base)%mod;
             }
             
+            
             vector<int> res;
-            
-            
+   
             for(int i = 0; i<=txt.size()-pat.size(); i++){ //i is start of current window
                 if(patHash == winHash && isEqual(txt, i, pat))
                     res.push_back(i+1);
                     
-                int oldCharVal = int(txt[i]);
-                int newCharVal = int(txt[i+len]);
-              
-                winHash = ((winHash - oldCharVal*wt)%mod + mod)%mod;
-                winHash = (winHash*base)%mod;
-                winHash = (winHash + newCharVal)%mod;
+                winHash -= (txt[i]*wt)%mod;
+                if(winHash < 0) winHash = winHash+mod;
+                winHash *= base; winHash %= mod;
+                winHash += txt[i+len]; winHash %= mod;
             }
             
             if(res.size() == 0) return {-1};
@@ -60,7 +58,7 @@ class Solution
             return res;
             
         }
-        //T - O(n*m)   //Avg case is far less O(n) < Tavg < O(n*m)
+        //T - O(n*m)   //Avg case is far less O(n) < Tavg < O(n*m). Where n = txtLength and m = patLength
         //S - O(1)
      
 };
