e6083ca97a6812d9c2be0cf68d7c4cc9e07ef81f
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-23 15:40:10 +0530

    Time: 9 ms (69.31%), Space: 6.6 MB (95.94%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int base = 256;
+    int mod = 1e9+7;
+    
+    bool isEqual(string &a, int& si, string &b){
+        for(int i = 0; i<b.size(); i++){
+            if(a[(i+si)%a.size()] != b[i])
+                return false;
+        }
+        
+        return true;
+    }
+    
+    int repeatedStringMatch(string a, string b) {
+        long bHash = 0, aHash = 0;
+        long wt = 1;
+        int blen = b.size();
+        
+        for(int i = b.size()-1; i>=0; i--){
+            bHash += wt*b[i]; bHash%=mod;
+            aHash += wt*a[i%a.size()]; aHash%=mod;
+            
+            if(i!=0)
+            wt *= base, wt%=mod;
+        }
+        
+        int indA = -1;
+        for(int i = 0; i<a.size(); i++){
+            if(bHash == aHash && isEqual(a, i, b)){
+                indA = i; break;
+            }
+            aHash -= (a[i]*wt)%mod;
+            if(aHash < 0) aHash += mod;
+            aHash *= base; aHash %= mod;
+            aHash += a[(i+blen)%a.size()]; aHash %= mod;
+        }
+        
+        if(indA == -1)  return indA;
+        else return (indA+blen-1)/a.size() + 1;
+    }
+    //T - O(alen*blen) in the worst case and O(alen+blen) in the avg case
+    //S - O(1)
+};
