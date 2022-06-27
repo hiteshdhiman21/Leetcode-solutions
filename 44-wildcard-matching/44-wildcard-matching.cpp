e1f149bc5900723cd733bcae506bc63e96d08c6a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-06-27 18:26:02 +0530

    Time: 117 ms (32.71%), Space: 9.4 MB (74.88%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -6,24 +6,19 @@ public:
         vector<vector<bool>> match(m+1, vector<bool>(n+1));
         
         match[m][n] = true;
-        bool isGoing = true;
-        for(int i=m-1; i>=0; i--){
-            if(p[i] != '*')
-                isGoing = false;
-            
-            if(isGoing)
-                match[i][n] = true;
-            for(int j=n-1; j>=0; j--){
-                if( (p[i] == '?' || p[i] == s[j]) && match[i+1][j+1])
+        for(int i=m; i>=0; i--){
+
+            for(int j=n; j>=0; j--){
+                if((p[i] == '?' || p[i] == s[j]) && i+1 <=m && j+1 <= n && match[i+1][j+1])
                     match[i][j] = true;
-                else if(p[i] == '*' && (i==m-1 || match[i+1][j] || match[i][j+1]))
+                else if(p[i] == '*' && (i+1 <= m && match[i+1][j] || j+1 <= n && match[i][j+1]))
                     match[i][j] = true;
             }
-        }
+        }       
         
-        /*for(int i = 0; i<=m; i++){
-            for(int j=0; j<=n; j++){
-                cout << match[i][j] << " " ;
+        /*for(int i =0; i<=m; i++){
+            for(int j =0; j<=n; j++){
+                cout << match[i][j] << " ";
             }
             cout << endl;
         }*/
