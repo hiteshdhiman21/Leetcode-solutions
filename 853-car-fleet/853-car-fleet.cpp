c2e4da0b957639f8ff3fad9c3d7f55045a766a2a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-02 18:09:59 +0530

    Time: 196 ms (98.53%), Space: 78.6 MB (57.14%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        
+      /*Fact: Every car max will only be be able to join the car fleet before it and not any other. Thererefore we will iterate rom the last, since the car at the last index(max position) will be before all the cars.
+        Step-1. Convert the storage format for {position, speed} so that we can sort according to position of each car.
+        Step-2. If the time taken for current car is > time taken for fleet before the current car. Then this car will not be able to join that fleet and Hence it will start its new fleet. Hence do cnt++ and for cars behind it update the lastFleetTimeTaken = curCarTimeTaken.
+                (we are using dist and speed for it to avoid precision error)
+        Step-3. Else, it means this car can join the fleet before it. Hence it will join it and  time taken for it will become equal to the time taken for fleet(Because it will slow down to speed of next fleet). Hence Don't update the cnt.
+        Step-4. Finally return the cnt.*/
+        
         int n = position.size();
         vector<pair<int,int>> posSpeed;
         
@@ -9,8 +17,6 @@ public:
         }
         
         sort(posSpeed.begin(), posSpeed.end());
-        
-        
         
         int cnt = 0;
         long lastDistRem = 0;
@@ -30,4 +36,7 @@ public:
         
         return cnt;
     }
+    //T - O(nlogn)
+    //S - O(n) or O(1) depending upon the input format
+    
 };
