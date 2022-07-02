7def647b5033fe52eb92d29b328d88e417fc2597
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-02 17:24:03 +0530

    Time: 3 ms (98.51%), Space: 7.1 MB (91.45%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,26 +2,33 @@ class Solution {
 public:
     bool checkInclusion(string& s1, string& s2) {
         vector<int> freq(26, 0);
+        
+        int cntMatchedAlphabets = 0;
         for(char &ch: s1)
             freq[ch-'a']++;
+        
+        for(int i =0; i<26; i++)
+            if(freq[i] == 0)
+                cntMatchedAlphabets++;
         
         int k = s1.size();
         
         for(int i = 0; i<s2.size(); i++){
             freq[s2[i]-'a']--;
-            if(i>=k)
-                freq[s2[i-k]-'a']++;
             
-            bool ok = true;
-            for(int i =0; i<26; i++)
-                if(freq[i] != 0)
-                    ok = false;
-                
-            if(ok) return true;
+            if(freq[s2[i]-'a'] == 0) cntMatchedAlphabets++;
+            
+            if(i>=k){
+                freq[s2[i-k]-'a']++;
+                if(freq[s2[i-k]-'a'] == 1)
+                    cntMatchedAlphabets--;
+            }
+            
+            if(cntMatchedAlphabets == 26) return true;
         }
         
         return false;
     }
-    //T - O(n)
+    //T - O(n) , Not O(26n) using this technique
     //S - O(26)
 };
