2a816f0c4d225f6fcf739eaa385eae024df941f3
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-02 21:00:15 +0530

    Time: 3 ms (85.81%), Space: 10 MB (95.64%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,9 +5,9 @@ public:
         
         while(l<r){
             int mid = (l+r)/2;
-            if(nums[mid] < nums[r]){
+            if(nums[mid] < nums[r]){ //Pivot can only be in [left, mid]
                 r = mid;
-            }else{
+            }else{ //Pivot can only be in [mid+1, end]
                 l = mid+1;
             }
         }
