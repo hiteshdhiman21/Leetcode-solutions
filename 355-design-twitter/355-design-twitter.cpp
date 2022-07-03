5e85b92d1eb00fa19295b715e852da03d1f555ec
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-04 00:58:50 +0530

    Time: 3 ms (58.20%), Space: 7.5 MB (16.47%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -4,60 +4,36 @@ using minHeap = priority_queue<int, vector<int>, greater<int>>;
 class Twitter {
 
 private:
-    int cntr;
-    unordered_map<int, int> mp; //To map cntr-tweetid
-    unordered_map<int, set<int>> setFor; //To map each userID with Feed
-    vector<set<int>> followedBy;
-    unordered_map<int, int> tweetVsUser;
-    unordered_map<int, set<int>> userVsTweet;
+    vector<vector<int>> timeline;
+    vector<set<int>> following; 
     
 public:
     Twitter() {
-        cntr = 0;
-        followedBy.resize(N+1);
+        following.resize(N+1);
     }
     
-    void postTweet(int userId, int tweetId) {//T - O(n*f + t*logn), where f = followers of user and t = total tweets of user
-        followedBy[userId].insert(userId);
-        for(int x: followedBy[userId]){
-            setFor[x].insert(cntr);
-            mp[cntr] = tweetId;    
-        }
-        
-        tweetVsUser[tweetId] = userId;
-        userVsTweet[userId].insert(cntr);
-        cntr++;
+    void postTweet(int userId, int tweetId) {
+        timeline.push_back({userId, tweetId});
     }
     
-    vector<int> getNewsFeed(int userId) {//T - O(10*logn) , n = totalFeedSizeOfFollower 
+    vector<int> getNewsFeed(int userId) {
         vector<int> res;
-        for(auto it = setFor[userId].rbegin(); it != setFor[userId].rend(); it++){
-            int cntr = *it;
-            res.push_back(mp[cntr]);
+        
+        for(int i = (int)timeline.size()-1; i>=0; i--){
+            if(timeline[i][0] == userId || following[userId].count(timeline[i][0]))
+                res.push_back(timeline[i][1]);
             
             if(res.size() == 10) break;
         }
         return res;
     }
     
-    void follow(int followerId, int followeeId) {//T - O(nlogn) , n = totalTweetsByFollowee
-        followedBy[followeeId].insert(followerId);
-        
-        for(int tweetCntr: userVsTweet[followeeId]){
-            setFor[followerId].insert(tweetCntr);
-        }
+    void follow(int followerId, int followeeId) {
+        following[followerId].insert(followeeId);
     }
     
-    void unfollow(int followerId, int followeeId) {//T - O(nlogn) , n = totalFeedSizeOfFollower
-        followedBy[followeeId].erase(followerId);
-        
-        vector<int> tweetsFromFolloweeId;
-        for(int tweetCntr: setFor[followerId]){
-            if(tweetVsUser[mp[tweetCntr]] == followeeId){
-                setFor[followerId].erase(tweetCntr);
-            }
-        }
-        
+    void unfollow(int followerId, int followeeId) {
+        following[followerId].erase(followeeId);
     }
 };
 
