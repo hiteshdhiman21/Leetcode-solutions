f461bc9940fe686398899fc7c8d1a5371b3e6b04
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-04 14:08:44 +0530

    Time: 43 ms (7.63%), Space: 7.8 MB (48.21%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     unordered_map <char, int> m = { {'I', 1},
-        {'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}};
+                                    {'V', 5},
+                                    {'X', 10},
+                                    {'L', 50},
+                                    {'C', 100},
+                                    {'D', 500},
+                                    {'M', 1000}};
         
                                 
     int romanToInt(string s) {
+        //Algorithm: As usually written in Large to Small order. So if order reverses for any 2 indices, do the special treatment. Else do the normal treatment.
+        //Used map to make code much more readable.
+        
         int ans = 0;
         for(int i = 0; i<s.size(); i++){
             if(i+1 < s.size() && m[s[i]] < m[s[i+1]]){
@@ -17,4 +25,6 @@ public:
         
         return ans;
     }
+    //T - O(n)
+    //S - O(1) as Map size is fixed
 };
