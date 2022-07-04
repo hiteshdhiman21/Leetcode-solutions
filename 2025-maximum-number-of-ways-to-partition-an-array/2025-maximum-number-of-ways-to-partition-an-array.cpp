4c3dff1754066483b4cc9f88d89666f733a01ecc
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-05 01:45:06 +0530

    Time: 1288 ms (42.85%), Space: 239.2 MB (92.31%) - LeetHub

---
+++ 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int waysToPartition(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        long totSum = 0;
+        unordered_map<long, int> suffSumCnt, prefSumCnt;
+        for(int i = n-1; i >= 0; i--){
+            totSum += nums[i];
+            suffSumCnt[totSum]++;
+        }
+        
+        int res = 0;
+        if(totSum%2 == 0){
+            res = suffSumCnt[totSum/2];
+            cout << res << " "<< nums[0] << " " << totSum/2 << endl;
+            if(totSum == 0)
+                res--;
+        }
+                
+            
+        
+        long suffSum = totSum;
+        long prefSum = 0;
+         
+        for(int i = 0; i<n; i++){
+            long totSumAfterUpdate = totSum-nums[i]+k;
+            suffSumCnt[suffSum]--;
+            if(totSumAfterUpdate%2 == 0){
+                int part2 = suffSumCnt[totSumAfterUpdate/2];
+                suffSumCnt[totSumAfterUpdate/2]; 
+                int part1 = prefSumCnt[totSumAfterUpdate/2];
+                int now = 0;
+                if(suffSum -nums[i] != totSumAfterUpdate/2 && prefSum+k == totSumAfterUpdate/2) now++;
+                //cout << i << " " <<part1 << " " << part2 << " " << now << endl;
+                res = max(res, now+part1+part2);
+            }
+            
+           
+            
+            prefSum += nums[i];
+            prefSumCnt[prefSum]++;
+            suffSum -= nums[i];
+        }
+        
+        return res;
+    }
+    //T - O(n)
+    //S - O(n)
+};
