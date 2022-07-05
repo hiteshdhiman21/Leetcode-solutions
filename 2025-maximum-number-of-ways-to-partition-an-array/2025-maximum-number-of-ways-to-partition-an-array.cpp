5a395db0d1f8ca9da4cd9e9310e265813550931e
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-05 17:50:47 +0530

    Time: 1467 ms (19.77%), Space: 239.1 MB (92.31%) - LeetHub

--- 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.cpp
+++ 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.cpp
@@ -1,42 +1,49 @@
 class Solution {
 public:
     int waysToPartition(vector<int>& nums, int k) {
-        
         int n = nums.size();
+        unordered_map<long, int> suffSumCnt;
         
-        long totSum = 0;
-        unordered_map<long, int> suffSumCnt, prefSumCnt;
-        for(int i = n-1; i >= 0; i--){
-            totSum += nums[i];
-            suffSumCnt[totSum]++;
+        long suffSum = 0;
+        for(int i = n-1; i>=0; i--){
+            suffSum += nums[i];
+            suffSumCnt[suffSum]++;
         }
         
-        int res = 0;
+        long totSum = suffSum;
+        int maxPartitions = 0;
+        
         if(totSum%2 == 0){
-            res = suffSumCnt[totSum/2];
-            cout << res << " "<< nums[0] << " " << totSum/2 << endl;
+            maxPartitions = suffSumCnt[totSum/2]; //1
             if(totSum == 0)
-                res--;
+                maxPartitions--; //0
         }
-        
-        long suffSum = totSum;
+    
+        unordered_map<long, int> prefSumCnt;
+        /*i
+        /PrefSum [0...i-1] , SuffSum [i+1, n]
+        [0, 1, 2, 3, 4, 5]
+        [4, 0, 3, 1] // k= 0
+               c*/
         long prefSum = 0;
-         
-        for(int i = 0; i<n; i++){
-            long totSumAfterUpdate = totSum-nums[i]+k;
+        for(int i =0; i<n; i++){
             suffSumCnt[suffSum]--;
+            
+            int totSumAfterUpdate = totSum-nums[i]+k;
             if(totSumAfterUpdate%2 == 0){
-                int part2 = suffSumCnt[totSumAfterUpdate/2];
-                int part1 = prefSumCnt[totSumAfterUpdate/2]; //No need of Now variable
-                res = max(res, part1+part2);
+                int var2 = suffSumCnt[totSumAfterUpdate/2];
+                int var1 = prefSumCnt[totSumAfterUpdate/2];
+                maxPartitions = max(maxPartitions, var1+var2);
             }
             
             prefSum += nums[i];
-            prefSumCnt[prefSum]++;
             suffSum -= nums[i];
+            prefSumCnt[prefSum]++;
+            
         }
         
-        return res;
+        return maxPartitions;
+        
     }
     //T - O(n)
     //S - O(n)
