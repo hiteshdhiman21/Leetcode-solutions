1bc4f5987fab28fa83c14d09affa8a67943703a3
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-09 01:24:15 +0530

    Time: 13 ms (23.61%), Space: 11.2 MB (89.52%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {      
-        
+        //Complex but very good problem
+      /*Buy[i] denotes some stock is buyed on day <= i and what is max net profit after keeping some stock(Most profitable priced stock).
+        Sell[i] denotes net profit after not keeping any stock on day i. It means not buying any stock on this day, if there is no kept stock or just 
+                selling the most profitable kept stock price on this day*/
         int n = prices.size();
         
         if(n<=1) return 0;
@@ -15,6 +18,7 @@ public:
         for(int i=2; i<n; i++){
             buy[i] = max(sell[i-2]-prices[i], buy[i-1]);
             sell[i] = max(buy[i-1]+prices[i], sell[i-1]);
+            cout << buy[i] << " " << sell[i] << endl;
         }
         
         return sell[n-1];
