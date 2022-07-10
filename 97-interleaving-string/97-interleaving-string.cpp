43a9658b3590d839e36ae1983d6088ce5e808c7c
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-11 00:04:06 +0530

    Time: 4 ms (70.45%), Space: 6.5 MB (67.65%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    vector<vector<int>> dp;
+    vector<vector<bool>> dp;
     
-    bool solve(string &s1, string &s2, string& s3, int i1, int i2){
+    /*bool solve(string &s1, string &s2, string& s3, int i1, int i2){
         if(i1 == s1.size() && i2 == s2.size())
             return true;
         
@@ -20,12 +20,26 @@ public:
         
         return dp[i1][i2] = res;
         
-    }
+    }*/
     
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size()+s2.size() != s3.size()) return false;
-        dp.assign(s1.size()+1, vector<int>(s2.size()+1, -1));
+        dp.assign(s1.size()+1, vector<bool>(s2.size()+1, false));
         
-        return solve(s1, s2, s3, 0, 0);
+        int n1 = s1.size(), n2 = s2.size();
+        dp[n1][n2] = true;
+        
+        for(int i=n1; i>=0; i--){
+            for(int j=n2; j>=0; j--){
+                
+                if(i<n1 && s1[i] == s3[i+j])
+                    dp[i][j] = dp[i+1][j];
+                if(j<n2 && s2[j] == s3[i+j])
+                    dp[i][j] = dp[i][j] | dp[i][j+1];
+                
+            }
+        }
+        
+        return dp[0][0];
     }
 };
