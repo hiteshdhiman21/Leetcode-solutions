794393e21db4702521a33a1a43bef1abbc7c2f5e
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-11 00:10:56 +0530

    Time: 5 ms (61.62%), Space: 6.2 MB (94.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
     
-    vector<vector<bool>> dp;
     
-    /*bool solve(string &s1, string &s2, string& s3, int i1, int i2){
-        if(i1 == s1.size() && i2 == s2.size())
-            return true;
-        
-        if(dp[i1][i2] != -1) return dp[i1][i2];
-        
-        bool res = false;
-        if(i1 < s1.size() && s3[i1+i2] == s1[i1]){
-            res = res | solve(s1, s2, s3, i1+1, i2);
-        }
-        
-        if(i2 < s2.size() && s3[i1+i2] == s2[i2]){
-            res = res | solve(s1, s2, s3, i1, i2+1);
-        }
-        
-        return dp[i1][i2] = res;
-        
-    }*/
+    
+    
     
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size()+s2.size() != s3.size()) return false;
-        dp.assign(s1.size()+1, vector<bool>(s2.size()+1, false));
+        vector<bool> dp(s2.size()+1);
         
         int n1 = s1.size(), n2 = s2.size();
-        dp[n1][n2] = true;
+        dp[n2] = true;
         
         for(int i=n1; i>=0; i--){
             for(int j=n2; j>=0; j--){
+                bool oldVal = dp[j];
+                dp[j] = false;
+                
+                if(i==n1 && j==n2)
+                    dp[j] = true;
                 
                 if(i<n1 && s1[i] == s3[i+j])
-                    dp[i][j] = dp[i+1][j];
+                    dp[j] = oldVal;
                 if(j<n2 && s2[j] == s3[i+j])
-                    dp[i][j] = dp[i][j] | dp[i][j+1];
+                    dp[j] = dp[j] | dp[j+1];
                 
             }
         }
         
-        return dp[0][0];
+        return dp[0];
     }
 };
