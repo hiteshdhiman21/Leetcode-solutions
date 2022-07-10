65153863a4475246cbbcaede06c9c6a656828141
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-11 01:41:10 +0530

    Time: 1145 ms (6.38%), Space: 10.4 MB (52.73%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -20,6 +20,9 @@ public:
     }
     
     int maxCoins(vector<int>& nums) {
+      /*Intuition: If we try some i to be first stone in [s,e], then subproblems for [s,i-1] and [i+1, e] are inter-related and so we can't call recursion separately for both paritions.
+        Instead, We will travel opposite in time and try to find ans. We can think it this way. Stones[s, e] are bursted except one and all the stones before s i.e, [, s-1] and after e i.e, [e+1, ] will be bursted in future. And so it means, currently those [, s-1] and [e+1, ] are present. And out of [s, e], we need to find which will be bursted last and all others have been bursted before(after in reverse time traversing). And so suppose if out of [s, e] we will burst some b now(it means last), it will have its neighbour as (s-1) and (e+1) as they will be bursted after it (before in reverse time traversing) and not in [s,e] as they will be bursted before(after in reverse time traversing). 
+        */
         vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1));
         return solve(nums, 0, nums.size()-1, dp);
     }
