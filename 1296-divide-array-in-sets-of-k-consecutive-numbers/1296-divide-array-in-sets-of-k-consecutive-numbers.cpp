3b33ca11ddc737bb7af20936e27e6e669711f90e
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-11 17:41:24 +0530

    Time: 345 ms (25.63%), Space: 51.2 MB (25.00%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     bool isPossibleDivide(vector<int>& nums, int k) {
-                
+      /*Approach: Greedy
+        Step-1. Make a freq map of nums elements so that we can take a minimum element for every round. And easily remove the taken element in O(logn)
+        Step-2. While freq.size() > 0, Take mn element from the map(as it will always start a new round). And check if k consecutive elements including mn are present in the array. If not. Return false.
+        Else go for another round.
+        Step-3. Finally if freq.size() == 0. It means that for every round there are k consecutive elements. Hence return true.*/
+        
         map<int, int> freq;
         for(int x: nums)
             freq[x]++;
