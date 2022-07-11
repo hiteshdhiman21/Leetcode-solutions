c6ffeb63f68821cd8b0ed99afac79498b1dea7b6
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-11 20:15:43 +0530

    Time: 578 ms (87.71%), Space: 146.5 MB (36.74%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+      /*Approach: Greedy 
+        Step-1. Choosing only those triplets which may not make the ans bad.
+        Step-2. Out of those triplets if target values are present in the corresponding indices
+                Then the ans is true. Else false*/
         bool f = false, s = false, t= false;
         for(auto& _: triplets){
             if(_[0] <= target[0] && _[1] <= target[1] && _[2] <= target[2]){
+                //Choosing only those triplets which will not make the ans bad
                 if(_[0] == target[0]) f = true;
                 if(_[1] == target[1]) s = true;
                 if(_[2] == target[2]) t = true;
