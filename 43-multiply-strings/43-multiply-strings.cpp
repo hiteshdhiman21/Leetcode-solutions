96b1c6b5b25652f40a5aa29d57aba3648a9499ac
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-12 16:55:50 +0530

    Time: 12 ms (53.82%), Space: 6.6 MB (83.25%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    void add(string& res, string& s){
-        int i = 0;
-        int c = 0;
-        
-        for(int i=0; i<res.size() || i<s.size() || c!=0; i++){
-            int sum = c;
-            if(i<res.size()) sum += res[i]-'0';
-            if(i<s.size()) sum += s[i]-'0';
-            
-            c = sum/10;
-            sum = sum%10;
-            if(i<res.size())
-            res[i] = sum+'0';
-            else
-            res.push_back(sum+'0');
-        }
-    }
     
     string multiply(string num1, string num2) {   
        
@@ -24,29 +7,31 @@ public:
         reverse(num1.begin(), num1.end());
         reverse(num2.begin(), num2.end());
         
-        string res = "";
+        string res(num1.size()+num2.size(), '0');
         
         for(int i=0; i<num2.size(); i++){
-            string row = "";
-            for(int time=0; time<i; time++) row += "0";
-            int c = 0;
-            for(int j=0; j<num1.size() || c!=0; j++){                
-                int prod = c;
-                if(j<num1.size()) prod += (num2[i]-'0')*(num1[j]-'0');
-                c = prod/10;
-                prod = prod%10;
-                cout << prod << endl;
-                row.push_back(prod+'0');
-               
-            }
+            int prodCarry = 0, sumCarry = 0;
             
-            add(res, row);
+            for(int j=0; j<num1.size() || prodCarry!=0 || sumCarry != 0; j++){ 
+                int prod = prodCarry;
+                if(j<num1.size()) prod += (num2[i]-'0')*(num1[j]-'0');
+                prodCarry = prod/10;
+                prod = prod%10;
+                
+                int sum = res[i+j]-'0' + sumCarry + prod;
+                sumCarry = sum/10;
+                sum = sum%10;
+                
+                res[i+j] = sum+'0';
+            }
         }
+        
+        while(res.size() > 1 && res.back() == '0') res.pop_back();
         
         reverse(res.begin(), res.end());
         return res;
     }
     //T - O(m*n)
-    //S - O(m+n)
+    //S - O(1)
     
 };
