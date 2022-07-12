72a76c52540d4632a66c78201e072f34c747b7e8
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-12 13:16:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.06%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -11,20 +11,20 @@ public:
         return num;
     }
     
+    unordered_set<int> s ={4, 16, 37, 58, 89, 145, 42, 20, 4};
     bool isHappy(int n) {
-      /*Approach: Floyd Warshall Hare and Tortoise cycle detection algorithm*/  
-        int fr = n, sr = n;
+      /*Approach: Just hardcode. Not expected in interview
+        There is only one Cycle: 4 -> 16 -> 37 -> 58 -> 89 -> 145 -> 42 ->20 -> 4.
+        If a number is one of them. Then it will result in this cycle. And hence will not be a happy number. or If a number reaches any one of this no. it will get in a cycle and is not a happy number.
+        See Leetcode Approach 3 and 4.
+        */  
         
-        do{
-            fr = next(next(fr));
-            sr = next(sr);
-        }while(fr != sr && sr != 1);
+        while(n!=1 && n!=4) n = next(n);
         
-        return (sr==1);
+        return n==1;        
     }
-    //T - nRounds*(logn + log(logn) + log(log(logn)) + log(log(log(logn)))) = nRounds*log(n) = log(n)
-    //Suppose there k steps in cycle. And SR-FR == k. It will take max k steps for both of them to coincide as FR get 1 step closer to SR in each step. Hence nRounds <= 2(actually just a little round more than 1). Hence max no of steps <= k steps.
+    //T - logn + log(logn) + log(log(logn)) + log(log(log(logn))) = log(n)
     //As 10^(n) will become <= 81*n, Hence n digit no. become <= n. Hence num become <= log10(num)
     //999999999 -> 1053, 9999 -> 324, 999 -> 243, 99 ->162, 9 -> 81. So, 9 digit num will become  3 digit in 2 steps and then it will stay <= 243.
-    //S - O(1)
+    //S - O(1) hardcoded. Not maintaining previous seen numbers.
 };
