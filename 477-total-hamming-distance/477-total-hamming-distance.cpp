5d11fa5e8f461eaff064a6dae05606f76ec9405a
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-14 15:25:29 +0530

    Time: 82 ms (48.19%), Space: 19.1 MB (56.18%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        
         int n = nums.size();
-        vector<int> dp(32, 0);
-        
-        for(int x: nums){
-            for(int b=0; b<32; b++){
-                if(x&(1<<b))
-                    dp[b]++;
-            }
-        }
-        
         int res = 0;
+        
         for(int b=0; b<32; b++){
-            res += (n-dp[b])*dp[b];
+            int setCnt = 0;
+            for(int x: nums)
+                if(x&(1<<b))
+                    setCnt++;
+            
+            res += setCnt*(n-setCnt);
         }
         
         return res;
     }
-    //T - O(n)
+    //T - O(32n)
     //S - O(1)
 };
