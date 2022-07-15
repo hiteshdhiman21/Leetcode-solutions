f39373fa51be34347084b07696a4917dc80eb6a9
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-15 17:22:58 +0530

    Time: 57 ms (93.81%), Space: 11.7 MB (89.30%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,50 +1,3 @@
-void buildTree(vector<int> &tree, int start, int end, int treeNode){
-    if(start == end){
-        tree[treeNode] = 1;
-        return;
-    }
- 
-    int mid = (start+end)/2;
-    buildTree(tree, start, mid, 2*treeNode);
-    buildTree(tree, mid+1, end, 2*treeNode+1);
- 
-    tree[treeNode] = tree[2*treeNode] + tree[2*treeNode+1];
-}
- 
-void updateTree(vector<int> &tree, int start, int end, int treeNode, int idx){
-    if(start == end){
-        tree[treeNode] = 0;
-        return;
-    }
- 
-    int mid = (start+end)/2;
- 
-    if(idx <= mid){
-        updateTree(tree, start, mid, 2*treeNode, idx);
-    }else{
-        updateTree(tree, mid+1, end, 2*treeNode+1, idx);
-    }
- 
-    tree[treeNode] = tree[2*treeNode] + tree[2*treeNode+1];
-}
-
-int QueryOnTree(vector<int> &tree, int start, int end, int treeNode, int noFrees){
-    if(start == end){
-        return start;
-    }else if(start > end){
-        return end;
-    }
-    
-    int mid = (start+end)/2;
- 
-    if(tree[2*treeNode] > noFrees){
-        return QueryOnTree(tree, start, mid, 2*treeNode, noFrees);
-    }else {
-        return QueryOnTree(tree, mid+1, end, 2*treeNode+1, noFrees-tree[2*treeNode]);
-    }
-}
- 
-
 class Solution {
 public:
     
@@ -56,19 +9,26 @@ public:
     }
 
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+      /*
+        Approach: Used segment tree.
+        Finding the leftmost index with > k free seats in [0...leftMostIndex]
+        Which means [0...leftMostIndex-1] have exactly k free seats
+        There is no valid ans for [[5,1], [5,1]] or [[6,1], [7,0], [5,2], [5,2]]*/
         int n = people.size();
         sort(people.begin(), people.end(), cmp);
-        vector<int> tree(4*n+1);
-        
-        buildTree(tree, 0, n-1, 1);
         
         vector<vector<int>> res(n);
         
         for(int i=0; i<n; i++){
             int h = people[i][0], k = people[i][1];
-            int ind = QueryOnTree(tree, 0, n-1, 1, k);
-            res[ind] = people[i];
-            updateTree(tree, 0, n-1, 1, ind);
+            int cnt = 0;
+            int j;
+            for(j=0; j<n; j++){
+                
+                if(res[j].size() == 0) cnt++;
+                if(cnt == k+1) break;
+            }
+            res[j] = people[i];
         }
         
         return res;
