7f64899288b6569423c01a5191d84b98a1e2ab7d
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-15 14:59:32 +0530

    Time: 43 ms (78.49%), Space: 12.6 MB (64.94%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int gcd(int a, int b) { 
+    if(b==0)
+        return a;
+    return gcd(b, a%b);
+    } 
+
+    int maxPoints(vector<vector<int>>& points) {
+        int res = 1;
+        
+        for(int i=0; i<points.size(); i++){
+            map<pair<int, int>, int> freqSlope;
+            for(int j=i+1; j<points.size(); j++){
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
+                int dx = (x2-x1), dy = (y2-y1);
+                if(dx < 0) dx = -dx, dy = -dy;
+                
+                int g = gcd(dx, dy);
+                dx /= g; dy /= g;
+                
+                freqSlope[{dx, dy}]++;
+                res = max(res, freqSlope[{dx, dy}]+1);
+            }
+        }
+        
+        return res;
+    }
+};
