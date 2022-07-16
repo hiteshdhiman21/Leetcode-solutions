44190e90cf4eb103c156656b101f87433f5a18c5
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-17 01:15:04 +0530

    Time: 1494 ms (26.94%), Space: 253.4 MB (26.67%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+      /*Step-1. First calc prefRowSum for each row in the matrix. matrix[r][c2] - matrix[r][c1-1] indicates sum of matrix[r][c1....c2].
+        Step-2. Iterate over all possible column combinations using c1 and c2. 
+        Step-3. For each particular c1, c2 combination, Iterate over all the rows and calculate prefSum of submatrices [{0, c1} and {r, c2}].
+                Also maintain a frequency map of prefMatrixSum. and use it to calculate no. of 2-D or 1-D submatrices in starting at c1 and ending                 at c2 with sum = target. 
+        Step-4. PrefMatrixSum[r] denotes sum [{0, c1} and {r, c2}]. PrefMatrixSum[r] - prefMatrixSum[l-1] denotes sum [{l, c1} and {r, c2}].
+                Hence we can easily obtain submatrices starting at c1 and ending at c2. PrefMatrixSum[r] = PrefMatrixSum[r-1]+prefRowSum[r].
+        Step-5. Using these facts calculate for each possible c1, c2 combinations, no. of submatrices(with c1, c2 same and only rows different)                     with sum = target.*/
+        
         int n = matrix.size(), m = matrix[0].size();
         for(int r=0; r<n; r++){
             for(int c=1; c<m; c++){
@@ -19,10 +27,11 @@ public:
                     res += freq[sum-target];
                     freq[sum]++;
                 }
-                //cout << c1 << " " << c2 << " " << sum << endl;
             }
         }
         
         return res;
     }
+    //T - O(m*m*n)
+    //S - O(n)
 };
