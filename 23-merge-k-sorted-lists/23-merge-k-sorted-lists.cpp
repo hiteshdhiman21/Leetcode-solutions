c3bdfedde6397cb9fcd2e6590f73b68263f08e68
Hitesh Dhiman <88251210+hiteshdhiman21@users.noreply.github.com>
2022-07-27 19:03:28 +0530

    Time: 39 ms (56.89%), Space: 13 MB (87.52%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -11,30 +11,43 @@
 class Solution {
 public:
     
-    priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq; 
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        if(l1->val> l2->val)
+            swap(l1, l2);
+        
+        ListNode* head = l1;
+        
+        
+        while(l1 && l2){
+            ListNode *tail = NULL;
+            while(l1 != NULL && l1->val <= l2->val){
+                tail = l1;
+                l1 = l1->next;
+            }
+            tail->next = l2;
+            swap(l1, l2);
+        }
+
+return head;
+    }
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *head = NULL, *tail = NULL;
+        if(lists.size() == 0) return NULL;
         
-        for(int i=0; i<lists.size(); i++){
-            if(lists[i] != NULL)
-                pq.push({lists[i]->val,lists[i]});
-        }
-            
-        
-        while(!pq.empty()){
-            auto[val, node] = pq.top(); pq.pop();
-            if(head == NULL) head = tail = node;
-            else{
-                tail->next = node;
-                tail = node;
+        int n = lists.size();
+        int interval = 1;
+        while(interval < n){
+            for(int i=0; i<n-interval; i+=2*interval){
+                lists[i] = mergeTwoLists(lists[i], lists[i+interval]);
             }
-            
-            if(node->next)
-                pq.push({node->next->val, node->next});
+            interval *= 2;
         }
         
-        return head;
+        return lists[0];
     }
     //T - O(nlogk) , where n = total nodes
     //S - O(k)
